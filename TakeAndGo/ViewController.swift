6468484f14d1b8be315bfbb5c170503c07c6036e
Okzhetpes <okhzetpes.20@gmail.com>
2020-10-22 16:42:11 +0600

    Initial Commit

---
+++ TakeAndGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeAndGo
+//
+//  Created by fsociety.3 on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
