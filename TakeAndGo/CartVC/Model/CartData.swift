5046dfec648d57a07e5b404d0ee139935017561a
Okzhetpes <okhzetpes.20@gmail.com>
2021-05-31 22:06:06 +0600

    Added missing file

--- TakeAndGo/CartVC/Model/CartData.swift
+++ TakeAndGo/CartVC/Model/CartData.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  CartData.swift
 //  TakeAndGo
 //
 //  Created by fsociety.3 on 10/22/20.
@@ -7,3 +7,15 @@
 
 import Foundation
 
+//struct CartData: Codable {
+//    var totalPrice: Int
+//    var shoppingCart: [Item]
+//}
+
+typealias CartData = [Item]
+
+
+struct Item: Codable {
+    let itemName: String
+    let itemPrice, itemWeight, count: Int
+}
